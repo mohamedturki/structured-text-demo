81bda0ae2a486f65363272af26dd9717d3aa89a1
Mohamed Turki <hi@mturki.me>
2018-08-01 15:51:06 +0200

    Fix formatting

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-require('dotenv').config()
-const contentful = require('contentful')
-const {documentToHtmlString} = require('@contentful/structured-text-html-serializer')
-const {BLOCKS} = require('@contentful/structured-text-types')
+require('dotenv').config();
+const contentful = require('contentful');
+const {
+  documentToHtmlString
+} = require('@contentful/structured-text-html-serializer');
+const { BLOCKS } = require('@contentful/structured-text-types');
 
-const accessToken = process.env.ACCESS_TOKEN
-const space = process.env.SPACE_ID
-const entryId = process.env.ENTRY_ID
+const accessToken = process.env.ACCESS_TOKEN;
+const space = process.env.SPACE_ID;
+const entryId = process.env.ENTRY_ID;
 
 const renderOptions = {
   renderNode: {
-    [BLOCKS.EMBEDDED_ENTRY]: (node) => `<code>${JSON.stringify(node, null, 2)}</code>`
+    [BLOCKS.EMBEDDED_ENTRY]: node =>
+      `<code>${JSON.stringify(node, null, 2)}</code>`
   }
-}
+};
 
 const client = contentful.createClient({
   space,
   accessToken
-})
+});
 
 // Use the collection endpoint but fetch only the
 // entry(ies) that you want to render.
-client.getEntries({include: 4, 'sys.id': entryId })
-  .then((entries) => {
+client
+  .getEntries({ include: 4, 'sys.id': entryId })
+  .then(entries => {
     const renderOptions = {
       renderNode: {
-        [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+        [BLOCKS.EMBEDDED_ENTRY]: node => {
           const entryId = node.data.target.sys.id;
-          let entry = {}
+          let entry = {};
           // look for the resolved entries either in the `includes` field
-          // (if it was part of the `.items`) 
+          // (if it was part of the `.items`)
           if (entries.includes) {
-            entry = entries.includes.Entry.find(e => e.sys.id === entryId)
+            entry = entries.includes.Entry.find(e => e.sys.id === entryId);
 
-          // or in the `.items` if it was returned as part of the queried entries.
+            // or in the `.items` if it was returned as part of the queried entries.
           } else {
-            entry = entries.items.find(e => e.sys.id === entryId)
+            entry = entries.items.find(e => e.sys.id === entryId);
           }
-          return `<code>${JSON.stringify(entry)}</code>`
+          return `<code>${JSON.stringify(entry)}</code>`;
         }
       }
-    }
+    };
     entries.items.filter(entry => entry.sys.id === entryId).forEach(entry => {
-      const structuredFields = Object.keys(entry.fields).filter(fieldName => entry.fields[fieldName].nodeClass).map(fieldName => entry.fields[fieldName])
+      const structuredFields = Object.keys(entry.fields)
+        .filter(fieldName => entry.fields[fieldName].nodeClass)
+        .map(fieldName => entry.fields[fieldName]);
       structuredFields.forEach(field => {
-        console.log(documentToHtmlString(field, renderOptions))
-      })
-    })
+        console.log(documentToHtmlString(field, renderOptions));
+      });
+    });
   })
-  .catch(console.error)
-
+  .catch(console.error);
